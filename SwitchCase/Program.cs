3028e2329497aefa0da19d6d8617e2d7f9d14352
mosamircs <mosamircs@gmail.com>
2021-05-13 01:48:22 +0200

    Add project files.

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int TotalCoffeCost = 0;
+            Console.WriteLine("Enter Your Coffe Size : \n 1 - Small \n 2 - Meduim \n 3 - Large");
+        }
+    }
+}
